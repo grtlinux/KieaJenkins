8592e3584626032857b6bddbae82570b25c0e74f
grtlinux <grtlinux@gmail.com>
2018-11-14 15:30:20 +0900

    create 1st

---
+++ ant01/ant-web/src/org/tain/antweb/AppMain.java
@@ -0,0 +1,8 @@
+package org.tain.antweb;
+
+public class AppMain {
+
+	public static void main(String[] args) {
+		System.out.println(">>>>> " + new Object() {}.getClass().getEnclosingClass().getName());
+	}
+}
---
+++ mvn01/mvn-web/src/test/java/org/tain/mvnweb/AppTest.java
@@ -0,0 +1,8 @@
+package org.tain.mvnweb;
+
+public class AppTest {
+
+	public static void main(String[] args) {
+		System.out.println(">>>>> " + new Object() {}.getClass().getEnclosingClass().getName());
+	}
+}
